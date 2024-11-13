3044d8e0009110fc046fd80c2bff2d30dbabd3f5
Willow Lin <willowlin@WillowdeMac-mini.local>
2024-11-13 16:38:31 +0800

    修改setup.py文件

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='litedis',  # 替换为你的包名
-    version='0.1.0',  # 替换为你的版本号
-    author='Your Name',  # 替换为你的名字
-    author_email='your_email@example.com',  # 替换为你的邮箱
-    description='A brief description of your package',  # 替换为你的包的描述
-    # long_description=open('README.md').read(),  # 从 README 文件中读取长描述
+    name='litedis',
+    version='1.0.0',
+    author='Linsuiyuan',
+    author_email='linsuiyuan@icloud.com',
+    description='Litedis 是一个轻量级的 模仿Redis 的本地实现，它实现了和 Redis 类似的功能',
+    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/your_package_name',  # 替换为你的项目网址
-    packages=find_packages(),  # 自动找到包
+    url='https://github.com/linsuiyuan/litedis',
+    packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.7',
-    install_requires=[
-    ],
+    python_requires='>=3.8',
+    install_requires=[],
 )
